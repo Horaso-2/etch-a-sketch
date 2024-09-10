b7032d5578238701ef67fd04d0a41e3949553452
Horaso-2 <mik_volkov@hotmail.com>
2024-09-10 23:30:04 +0100

    Add grid and colouration

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-container = document.querySelector('.container')
+container = document.querySelector('.container');
+container.style.border = '3px solid black';
 
 let dim = 16;
 
@@ -10,11 +11,24 @@ for (let i=0; i<16; i++) {
     }
 }
 
-const square_dim = 960/dim;
+const square_dim = 880/dim;
 const squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
     square.style.display = 'flex';
     square.style.width = `${square_dim}px`;
     square.style.height = `${square_dim}px`;
     // square.style.flex = '';
+    square.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+
+    square.addEventListener('mouseover', () => {
+        increaseOpacity(square);
+    });
+    square.style.opacity = 0;
 });
+
+
+
+let increaseOpacity = (element) => {
+    const newOpacity = +getComputedStyle(element).opacity + 0.1;
+    element.style.opacity = `${newOpacity}`;
+}
